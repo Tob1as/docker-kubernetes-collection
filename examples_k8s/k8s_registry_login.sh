a7ca3d7f93b1bb75fbdde5a049e4b0fb59d4f135
Tobias Hargesheimer <tobias@ison.ws>
2024-04-04 00:18:55 +0200

    .

---
+++ examples_k8s/k8s_registry_login.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# https://kubernetes.io/docs/tasks/configure-pod-container/pull-image-private-registry/#create-a-secret-by-providing-credentials-on-the-command-line
+
+CONTAINER_REGISTRY='https://index.docker.io/v1/'  # for DockerHub use: https://index.docker.io/v1/
+CONTAINER_USER_NAME='<your-name>'
+CONTAINER_USER_PASSWORD='<your-pword>'
+CONTAINER_USER_EMAIL='<your-email>'
+
+K8S_REGCRED_NAME='regcred'
+
+# create regcred for specific namespace
+#K8S_NAMESPACE='default'
+#kubectl --namespace="${K8S_NAMESPACE}" create secret docker-registry ${K8S_REGCRED_NAME} --docker-server="${CONTAINER_REGISTRY}" --docker-username="${CONTAINER_USER_NAME}" --docker-password="${CONTAINER_USER_PASSWORD}" --docker-email="${CONTAINER_USER_EMAIL}"
+
+# or:
+
+# create regcred for all namespaces
+for ns in $(kubectl get namespaces -o=jsonpath='{.items[*].metadata.name}'); do
+    kubectl --namespace="$ns" create secret docker-registry ${K8S_REGCRED_NAME} \
+        --docker-server="${CONTAINER_REGISTRY}" \
+        --docker-username="${CONTAINER_USER_NAME}" \
+        --docker-password="${CONTAINER_USER_PASSWORD}" \
+        --docker-email="${CONTAINER_USER_EMAIL}"
+done
